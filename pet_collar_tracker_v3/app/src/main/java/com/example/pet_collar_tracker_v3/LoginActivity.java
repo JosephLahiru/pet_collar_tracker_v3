d659e37924947f33219dabfd2745ba7caa46f958
rasinduravisara <43920667+rasinduravisara@users.noreply.github.com>
2021-09-25 14:07:14 +0530

    Add Firebase Libraries

--- pet_collar_tracker_v3/app/src/main/java/com/example/pet_collar_tracker_v3/LoginActivity.java
+++ pet_collar_tracker_v3/app/src/main/java/com/example/pet_collar_tracker_v3/LoginActivity.java
@@ -2,14 +2,48 @@ package com.example.pet_collar_tracker_v3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText userName, Pwd;
+    Button Login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+<<<<<<< Updated upstream
+=======
+        userName = findViewById(R.id.username_data);
+        Pwd = findViewById(R.id.user_password);
+        Login_btn = findViewById(R.id.sign_in_button);
+
+
+        Login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (userName.equals("") || Pwd.equals("")) {
+                    Toast.makeText(LoginActivity.this, "Please enter values for all fields.", Toast.LENGTH_SHORT).show();
+                }
+                /*else {
+                    Boolean checkUserPass = database.authUser(user, pass);
+                    if (checkUserPass) {
+                        Intent intent = new Intent(getApplicationContext(), chat.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(this, "Invalid credentials.", Toast.LENGTH_SHORT).show();
+                    }*/
+            }
+        });
+>>>>>>> Stashed changes
     }
+
+
 }
