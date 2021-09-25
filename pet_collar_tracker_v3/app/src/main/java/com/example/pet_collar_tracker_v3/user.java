36144c584780d2a25c1733e7921a9aea093a79d8
rasinduravisara <43920667+rasinduravisara@users.noreply.github.com>
2021-09-25 23:23:00 +0530

    SignUp and SignIn functions
    SignUp and SignIn functions created. Need optimize and create normal user UI

--- pet_collar_tracker_v3/app/src/main/java/com/example/pet_collar_tracker_v3/user.java
+++ pet_collar_tracker_v3/app/src/main/java/com/example/pet_collar_tracker_v3/user.java
@@ -1,4 +1,21 @@
 package com.example.pet_collar_tracker_v3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class user {
+
+    public String usrName,deviceCode;
+    public List<String> deviceCodes = new ArrayList<>();
+
+    public user(){
+
+    }
+
+    public user(String usrName, String deviceCode){
+        this.usrName = usrName;
+        this.deviceCodes.add(deviceCode);
+
+    }
+
 }
