5b9ca3d29f1bebd9d7523112eeb44842ea8f0cd0
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2021-09-26 23:34:35 +0530

    finished 3.1 and 3.2

--- pet_collar_tracker_v3/app/src/main/java/com/example/pet_collar_tracker_v3/UserDeviceCurrentLocationActivity.java
+++ pet_collar_tracker_v3/app/src/main/java/com/example/pet_collar_tracker_v3/UserDeviceCurrentLocationActivity.java
@@ -1,14 +1,128 @@
 package com.example.pet_collar_tracker_v3;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class UserDeviceCurrentLocationActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Random;
+
+public class UserDeviceCurrentLocationActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    int[] pointerColors = new int[]{72, 144, 216, 288, 359};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_device_current_location);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+
+        TextView liveLocationTextView = (TextView) findViewById(R.id.liveLocationTextView);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("liveLocation");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                try {
+                    mMap.clear();
+
+                    int i=0; //TODO Change the i with logged in user's Device ID
+
+                    String latitude = dataSnapshot.child("Dev"+i).child("latitude").getValue().toString();
+                    String longitude = dataSnapshot.child("Dev"+i).child("longitude").getValue().toString();
+
+                    LatLng latLng = new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
+
+                    liveLocationTextView.setText("LIVE LOCATION : Dev" + i);
+
+                    mMap.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .title("Device " + i + " : " + latitude + " , " + longitude)
+                            .icon(BitmapDescriptorFactory.defaultMarker(pointerColors[i])));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                } catch (Exception e) {
+                    Toast.makeText(UserDeviceCurrentLocationActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            long MIN_DIST = 5;
+            long MIN_TIME = 1000;
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DIST, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DIST, locationListener);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- pet_collar_tracker_v3/app/src/main/java/com/example/pet_collar_tracker_v3/UserPanelActivity.java
+++ pet_collar_tracker_v3/app/src/main/java/com/example/pet_collar_tracker_v3/UserPanelActivity.java
@@ -2,7 +2,11 @@ package com.example.pet_collar_tracker_v3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class UserPanelActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class UserPanelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_panel);
+
+        Button userDeviceLocationHistoryBtn = (Button) findViewById(R.id.deviceLocationHistoryButton);
+        Button userDeviceCurrentLocationBtn = (Button) findViewById(R.id.deviceCurrentLocationButton);
+        TextView currentUserTextView = (TextView) findViewById(R.id.currentUserTextView);
+
+        currentUserTextView.setText("Currently logged as Karen"); //TODO get the user list and show user name
+
+        userDeviceLocationHistoryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchUserDeviceCurrentLocationHistoryActivity();
+            }
+        });
+
+        userDeviceCurrentLocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchUserDeviceCurrentLocationActivity();
+            }
+        });
+    }
+
+    public void launchUserDeviceCurrentLocationHistoryActivity(){
+        Intent userDeviceLocationHistoryIntent = new Intent(this, UserDeviceLocationHistoryActivity.class);
+        startActivity(userDeviceLocationHistoryIntent);
+    }
+
+    public void launchUserDeviceCurrentLocationActivity(){
+        Intent userDeviceCurrentLocationIntent = new Intent(this, UserDeviceCurrentLocationActivity.class);
+        startActivity(userDeviceCurrentLocationIntent);
     }
 }
