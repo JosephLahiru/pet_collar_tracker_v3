3f9419d15e1819fdbb3f405b4e08208fc0235de9
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2021-09-26 22:42:01 +0530

    added 3.1 and 3.2

---
+++ pet_collar_tracker_v3/app/src/main/java/com/example/pet_collar_tracker_v3/UserDeviceLocationHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.pet_collar_tracker_v3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserDeviceLocationHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_device_location_history);
+    }
+}
