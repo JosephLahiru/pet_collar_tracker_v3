7ca1fc5cc22dd18b971517e98b230001ebb37473
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2021-10-03 01:56:03 +0530

    added complaint form

---
+++ pet_collar_tracker_v3/app/src/main/java/com/example/pet_collar_tracker_v3/ComplaintFormActivity.java
@@ -0,0 +1,14 @@
+package com.example.pet_collar_tracker_v3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComplaintFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint_form);
+    }
+}
