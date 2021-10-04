8f83f4a20807f0f035f5cccae51a5b4b80203728
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2021-10-05 01:07:06 +0530

    fixing device managemnt

--- pet_collar_tracker_v3/app/src/main/java/com/example/pet_collar_tracker_v3/DeviceManagementActivity.java
+++ pet_collar_tracker_v3/app/src/main/java/com/example/pet_collar_tracker_v3/DeviceManagementActivity.java
@@ -27,17 +27,14 @@ public class DeviceManagementActivity extends AppCompatActivity {
 
         TableLayout tl = (TableLayout)findViewById(R.id.tableLayout2);
 
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("UserData");
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("liveLocation");
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 try{
-                    String databaseDevicesString = snapshot.child("Devices").getValue().toString().substring(1, snapshot.child("Devices").getValue().toString().length() - 1);
-                    String databaseUsersString = snapshot.child("Users").getValue().toString().substring(1, snapshot.child("Users").getValue().toString().length() - 1);
+                    String databaseDevicesString = snapshot.getValue().toString().substring(1, snapshot.getValue().toString().length() - 1);
 
                     String[] Devices = databaseDevicesString.split(", ");
-
-                    String[] Users = databaseUsersString.split(", ");
 
                     TextView[] textArray = new TextView[Devices.length];
                     TableRow[] tr_head = new TableRow[Devices.length];
@@ -46,9 +43,8 @@ public class DeviceManagementActivity extends AppCompatActivity {
 
                     for(int i=0; i<Devices.length; i++){
                         String curr_dev = Devices[i];
-                        String curr_user = Users[i];
 
-                        String[] data = new String[]{curr_user, curr_dev};
+                        String[] data = new String[]{curr_dev};
 
                         tr_head[i] = new TableRow(DeviceManagementActivity.this);
                         tr_head[i].setId(i+1);
